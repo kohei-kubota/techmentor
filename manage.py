39412ef555815b62f370f420622784f55986e49c
久保田公平 <kubotakouhei@kubotakouhei-no-MacBook-Pro.local>
2017-02-09 21:56:46 +0900

    sort

--- manage.py
+++ manage.py
@@ -3,8 +3,6 @@ import os
 import sys
 import pymysql
 
-pymysql.install_as_MySQLdb()
-
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "techmentor.settings")
     try:
@@ -23,3 +21,5 @@ if __name__ == "__main__":
             )
         raise
     execute_from_command_line(sys.argv)
+
+pymysql.install_as_MySQLdb()
