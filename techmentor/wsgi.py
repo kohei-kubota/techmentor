817cff792ea6f6a656f338f9f78629e83516e918
久保田公平 <kubotakouhei@kubotakouhei-no-MacBook-Pro.local>
2017-02-09 22:21:46 +0900

    undo

--- techmentor/wsgi.py
+++ techmentor/wsgi.py
@@ -18,6 +18,3 @@ application = get_wsgi_application()
 # Use whitenoise to serve static file on Heroku
 from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
-
-import pymysql
-pymysql.install_as_MySQLdb()
