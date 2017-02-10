2c405b5537de05c4f254af02ed9aa360233a3459
久保田公平 <kubotakouhei@kubotakouhei-no-MacBook-Pro.local>
2017-02-10 23:52:37 +0900

    django-strage

--- techmentor/settings.py
+++ techmentor/settings.py
@@ -128,7 +128,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 # To serve static files on Heroku
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -181,14 +181,17 @@ DATABASES['default'].update(db_from_env)
 # MEDIA_URL = '/media/'
 MEDIA_URL = 'https://s3-ap-northeast-1.amazonaws.com/techmentor-django-storage/'
 
+AWS_ACCESS_KEY_ID = 'AKIAJJ2Z3YF4AJCVYF5A'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'techmentor-django-storage'
 # collectstaic時にS3を使う
 MEDIAFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-AWS_STORAGE_BUCKET_NAME = 'techmentor-django-storage'
 # これをTrueにしたほうがファイル変更のチェックが速くなる
 AWS_PRELOAD_METADATA = True
 
-
-
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
 TINYMCE_SPELLCHECKER = True
 TINYMCE_COMPRESSOR = True
