70d6b94e37ed8120e4719b304723d30b4ee75438
久保田公平 <kubotakouhei@kubotakouhei-no-MacBook-Pro.local>
2017-02-13 17:27:20 +0900

    check profile-photo

--- techmentor/settings.py
+++ techmentor/settings.py
@@ -241,11 +241,11 @@ PAGINATION_SETTINGS = {
     'SHOW_FIRST_PAGE_WHEN_INVALID': True,
 }
 
-# MIDDLEWARE_CLASSES = [
-#     'lib.middleware.RedirectCorrectHostname',
-# ]
+MIDDLEWARE_CLASSES = [
+    'lib.middleware.RedirectCorrectHostname',
+]
 
-# CORRECT_HOST = 'www.techmentor.jp/'
+CORRECT_HOST = 'www.techmentor.jp/'
 
 
 
